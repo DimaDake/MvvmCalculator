4ebeaea6ae1ad62b25c510c282132e6defa64ac6
BrainLux <Veselblu@yandex.ru>
2019-05-21 11:01:49 +0300

    base functional

--- app/src/main/java/com/example/myapplication/CalculatorActivity.java
+++ app/src/main/java/com/example/myapplication/CalculatorActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private CalculatorViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(CalculatorViewModel.class);
+        final Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String value) {
+                ((EditText) findViewById(R.id.result)).setText(value);
+            }
+        };
+        viewModel.getScreenLiveData().observe(this, observer);
+    }
+
+    public void onButtonClick(View view) {
+        Button button = (Button) view;
+        switch (button.getText().toString()) {
+            case "+":
+                viewModel.onPlusButtonClicked();
+                break;
+            case "-":
+                viewModel.onMinusButtonClicked();
+                break;
+            case "=":
+                viewModel.onEqualsButtonClicked();
+                break;
+            case "AC":
+                viewModel.onAcButtonClicked();
+                break;
+            default:
+                viewModel.onDigitButtonClicked(button.getText().charAt(0));
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/CalculatorViewModel.java
+++ app/src/main/java/com/example/myapplication/CalculatorViewModel.java
@@ -1,31 +1,82 @@
 package com.example.myapplication;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class CalculatorViewModel extends ViewModel {
 
+    private final CalculatorModel model;
+    private MutableLiveData<String> screenLiveData;
+    private Mode mode;
+
+    private enum Mode {
+        ADDICTION,
+        SUBTRACTION
+    }
+
+    public CalculatorViewModel() {
+        this(new CalculatorModel());
+    }
+
+    public CalculatorViewModel(CalculatorModel model) {
+        this.model = model;
+        screenLiveData = new MutableLiveData<>();
+        screenLiveData.setValue("");
+    }
+
     public LiveData<String> getScreenLiveData() {
-        throw new UnsupportedOperationException("Implement this method!");
+        return screenLiveData;
     }
 
     public void onDigitButtonClicked(char digit) {
-        throw new UnsupportedOperationException("Implement this method!");
+        screenLiveData.setValue(screenLiveData.getValue() + digit);
     }
 
     public void onPlusButtonClicked() {
-        throw new UnsupportedOperationException("Implement this method!");
+        if (mode == null)
+            model.addition(getValue());
+        mode = Mode.ADDICTION;
+        model.setSubDigit(null);
+        screenLiveData.setValue("");
+    }
+
+    private long getValue() {
+        return Long.parseLong(screenLiveData.getValue());
     }
 
     public void onMinusButtonClicked() {
-        throw new UnsupportedOperationException("Implement this method!");
+        if (mode == null)
+            model.subtraction(getValue());
+        mode = Mode.SUBTRACTION;
+        model.setSubDigit(null);
+        screenLiveData.setValue("");
     }
 
     public void onEqualsButtonClicked() {
-        throw new UnsupportedOperationException("Implement this method!");
+        switch (mode) {
+            case ADDICTION:
+                if (model.getSubDigit() == null) {
+                    model.setSubDigit(getValue());
+                }
+                model.addition(model.getSubDigit());
+                screenLiveData.setValue(String.valueOf(model.getResult()));
+                break;
+            case SUBTRACTION:
+                if (model.getSubDigit() == null) {
+                    model.setSubDigit(getValue());
+                }
+                model.subtraction(model.getSubDigit());
+                screenLiveData.setValue(String.valueOf(model.getResult()));
+                break;
+            default:
+
+        }
     }
 
     public void onAcButtonClicked() {
-        throw new UnsupportedOperationException("Implement this method!");
+        screenLiveData.setValue(String.valueOf(CalculatorModel.INIT_VALUE));
+        model.reset();
+        mode = null;
     }
 }
